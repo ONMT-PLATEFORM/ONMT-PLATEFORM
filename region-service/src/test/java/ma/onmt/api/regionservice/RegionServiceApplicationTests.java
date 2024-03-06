69e3168088648aa409bc11ca11ee3a8cd0dd04c7
e.elhjoujy <e.elhjoujy@labelvie.ma>
2024-03-06 10:21:46 +0100

    ONMT-1-au-tant-quutilisateur-je-peux-consulter-les-regions-y-compris-leurs-attractions-touristiques-evenements-hebergements-et-ses-activites

---
+++ region-service/src/test/java/ma/onmt/api/regionservice/RegionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.onmt.api.regionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
