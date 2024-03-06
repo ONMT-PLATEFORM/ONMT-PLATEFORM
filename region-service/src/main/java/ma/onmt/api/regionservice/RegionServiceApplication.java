9b5d5f4a63f9d5be79ecb7306be645af2976d62e
e.elhjoujy <e.elhjoujy@labelvie.ma>
2024-03-06 20:16:57 +0100

    ADDING MORE ENDPOINT FOR THE USER'S ON THE REGION , WE NEED IN THIS CASE TO CALL USER'S ON THE SAME REGION

--- region-service/src/main/java/ma/onmt/api/regionservice/RegionServiceApplication.java
+++ region-service/src/main/java/ma/onmt/api/regionservice/RegionServiceApplication.java
@@ -5,11 +5,13 @@ import ma.onmt.api.regionservice.repository.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableDiscoveryClient
 public class RegionServiceApplication {
 
 	public static void main(String[] args) {
